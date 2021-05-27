7776e816ebdd8f4b451821fb41bb99fe010ca129
Martinfbr <m.felipe59@hotmail.com>
2021-05-27 15:33:14 -0500

    added Hero and to update app.Css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import React , {useContext, useState, useRef, useCallback, useEffect} from 'react'
 import Header from './components/header/Header';
-
-
+import Nav from './components/nav/Nav';
+import {Context} from './components/Context';
+import useEventListener from './components/UseEventListener'
+import Hero from './components/hero/Hero';
 
 function App() {
+
+  const {menuOpen} = useContext(Context)
+  const {toggleMenu} = useContext(Context)
+
+  function checkViewWidth() {
+    if(window.innerWidth >= 1100 && menuOpen) {
+      toggleMenu()
+    }
+  }
+  useEventListener('resize', checkViewWidth)
 
   return (
     <div className="App">
       <Header/>
-      {/* {
+      { 
         menuOpen && <Nav/>
       }
+      
       <Hero/>
-      <Shorten/>
+     {/* <Shorten/>
       <Features/>
       <Boost/>
       <Footer/> */}
