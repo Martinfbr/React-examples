e9607ccf7d20edc491eaa018410d64da5f43c081
Martinfbr <m.felipe59@hotmail.com>
2021-06-03 17:49:41 -0500

    API shorten and resultados

--- src/components/shorten/Shorten.js
+++ src/components/shorten/Shorten.js
@@ -1,8 +1,50 @@
 import React, {useState, useEffect, useContext} from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import './shorten.css'
+import {trackPromise} from 'react-promise-tracker'
 
+
+//API SHORTEN 
 function Shorten() {
+     
+    // inputs 
+    const [userInput, setUserInput] = useState('')
+    const [redInput, setRedInput] = useState(false)
 
+
+
+    function handleChange(e) {
+        const {value} = e.target
+        setUserInput(value)
+
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        if(userInput === '') {
+            setRedInput(true)
+        } else {
+            setRedInput(false)
+            trackPromise(
+                fetch(`https://api.shrtco.de/v2/shorten?url=${userInput}`)
+                .then(res => res.json())
+                .then(data => {
+
+                    localStorage.setItem(uuidv4(), JSON.stringify({
+                        original: userInput,
+                        shortUrl: data.result.full_short_link
+                    }))
+
+                    window.location.reload(true)
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+            )
+        }
+    }
+    
     return (
         <>
             <div className='shorten-outer-container'>
@@ -10,27 +52,25 @@ function Shorten() {
                     <div className='shorten-section-bg'></div>
                     <form 
                         className='form'
-                     
+                        onSubmit={handleSubmit}
                     >
                         <input 
-                            className={'user-input active'}
-                            type="text" 
-                            placeholder='Shorten a link here...'
-                            name='userInput'
-                         
+                        className={redInput ? 'user-input active' :'user-input' }
+                        type="text"
+                        placeholder="shorten link at..."
+                        id="shortenInput"
+                        onChange={handleChange}     
                         />
-
                         {
-                             <p className='error-message'>Please add a link</p> 
+                             <p className='error-message'>Please add a link</p>
                         }
-
                         <button 
                             id='shorten-btn'
                         >
                                 Shorten it!
                         </button>
-                      
-                    </form>
+                          
+                     </form>
                 </div>
             </div>
         </>
